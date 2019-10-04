ce6377933aa8745217ffbe9f0bfe2bb712c6380f
aliahmadcse <aliahmadcse@gmail.com>
2019-10-04 16:01:34 +0500

    Adds initial php files,like login.php, registration.php, etc

---
+++ README.md
@@ -0,0 +1,8 @@
+# Library Management System (LMS)
+
+This project is a my term project for the course of Software Engineering.
+
+Collaborative Members are:
+
+- Asad Ul Rehman
+- Haseeb Asif
---
+++ userAccount/userLogin.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- CDN for bootstrap -->
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <!-- Attaching css file -->
+    <link rel="stylesheet" href="css/userLogin.css" />
+    <title>Login</title>
+  </head>
+  <body>
+    <div class="fluid-container">
+      <!-- Responsive Bootstrap Navigation Bar -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="../index.html"
+          ><strong>LOGO Here</strong></a
+        >
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto" id="nav-list">
+            <li class="nav-item">
+              <a class="nav-link" href="../index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link-2</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link-3</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <!-- login form -->
+      <form class="login">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control user-email"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            required
+          />
+          <small id="emailHelp" class="form-text text-muted"
+            >We'll never share your email with anyone else.</small
+          >
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control user-password"
+            id="exampleInputPassword1"
+            placeholder="Password"
+            required
+          />
+        </div>
+
+        <button type="submit" class="btn btn-primary btn-block" id="btn-login">
+          Login
+        </button>
+        <div class="text-center" id="forgot-password-link">
+          <a href="forgotPassword.php" class="text-info">Forgot Password?</a>
+        </div>
+        <div class="text-center" id="user-registration-link">
+          <a href="userRegistration.php" class="text-info"
+            >Not registered yet!</a
+          >
+        </div>
+      </form>
+      <footer class="footer">
+        <small>&copy; Copyright 2019, Library Management System</small>
+      </footer>
+    </div>
+    <!-- Bootstrap CDN-->
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous"
+    ></script>
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+  </body>
+</html>
